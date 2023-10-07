880db42c46c4853ae2bbb58afe2ce7262d49db2c
Stas-Shlyckov-1988 <stas.shlyckov@yandex.ru>
2023-10-07 09:50:01 +0300

    frame

--- src/test2_opencv/Simple.java
+++ src/test2_opencv/Simple.java
@@ -1,11 +1,11 @@
 package test2_opencv;
 
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
 
 import org.opencv.core.Point3; 
+
+import javax.swing.JFrame;
 
 public class Simple {
 
@@ -15,8 +15,7 @@ public class Simple {
 		 System.out.println(Core.VERSION_MINOR); // 3 
 		 System.out.println(Core.VERSION_REVISION); // 0 
 		 System.out.println(Core.NATIVE_LIBRARY_NAME); // opencv_java330 
-		 //System.out.println(Core.getBuildInformation()); 
-		 //Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
+
 		 System.out.println("Point 3D"); 
 		 Point3 p = new Point3(); 
 		 System.out.println(p); // {0.0, 0.0, 0.0} 
@@ -29,8 +28,13 @@ public class Simple {
 		 System.out.println("Point 3D 2");
 		 Point3 p2 = new Point3(10.0, 20.0, 30.0); 
 		 System.out.println(p2.x + " " + p2.y + " " + p2.z); // 10.0 20.0 30.0 
+		 		 
+		 int w = 1000, h = 600; 
+		 JFrame window = new JFrame("Java load opencv"); 
+		 window.setSize(w, h); 
+		 window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		 
-		 
-		
+		 window.setLocationRelativeTo(null); 
+		 window.setVisible(true); 
 	}
 }
