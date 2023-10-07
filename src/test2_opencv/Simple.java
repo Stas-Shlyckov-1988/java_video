3847d35d3872bcbc22b5e142cd8b0bfb921191a2
Stas-Shlyckov-1988 <stas.shlyckov@yandex.ru>
2023-10-07 09:10:05 +0300

    add Points 3D

--- src/test2_opencv/Simple.java
+++ src/test2_opencv/Simple.java
@@ -5,6 +5,8 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
 
+import org.opencv.core.Point3; 
+
 public class Simple {
 
 	public static void main(String[] args) {
@@ -15,6 +17,20 @@ public class Simple {
 		 System.out.println(Core.NATIVE_LIBRARY_NAME); // opencv_java330 
 		 //System.out.println(Core.getBuildInformation()); 
 		 //Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
+		 System.out.println("Point 3D"); 
+		 Point3 p = new Point3(); 
+		 System.out.println(p); // {0.0, 0.0, 0.0} 
+		 System.out.println(p.x + " " + p.y + " " + p.z); // 0.0 0.0 0.0 
+		 p.x = 20.0; 
+		 p.y = 3.0; 
+		 p.z = 12.0; 
+		 System.out.println(p.x + " " + p.y + " " + p.z); // 20.0 3.0 12.0 
+		 
+		 System.out.println("Point 3D 2");
+		 Point3 p2 = new Point3(10.0, 20.0, 30.0); 
+		 System.out.println(p2.x + " " + p2.y + " " + p2.z); // 10.0 20.0 30.0 
+		 
+		 
 		
 	}
 }
