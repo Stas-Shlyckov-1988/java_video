24b1e5ffcd3e6b00555c884099bc9767adb43be5
Stas-Shlyckov-1988 <stas.shlyckov@yandex.ru>
2023-10-30 20:18:04 +0300

    format code

--- src/test2_opencv/Simple.java
+++ src/test2_opencv/Simple.java
@@ -1,45 +1,29 @@
 package test2_opencv;
 
-import javax.swing.JFrame;
+//import javax.swing.JFrame;
 
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.Point3;
 import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc; 
+
 
 public class Simple {
+	static {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
 
 	public static void main(String[] args) {
-		 System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		
-		 Mat m = Imgcodecs.imread("./i.jpeg");
-		 System.out.println("Welcome to opencv" + Core.VERSION);
-		 System.out.println(Core.VERSION_MAJOR); // 3 
-		 System.out.println(Core.VERSION_MINOR); // 3 
-		 System.out.println(Core.VERSION_REVISION); // 0 
-		 System.out.println(Core.NATIVE_LIBRARY_NAME); // opencv_java330 
+		 
+		Mat img = Imgcodecs.imread("C:/Users/stass/eclipse-workspace/test2_opencv/src/i.jpeg"); 
+		if (img.empty()) { 
+		 System.out.println("Не удалось загрузить изображение"); 
+		 return; 
+		} 
+		System.out.println(img.width()); 
+		System.out.println(img.height()); 
+		System.out.println(CvType.typeToString(img.type())); 
+		System.out.println(img.channels()); 
 
-		 System.out.println("Point 3D"); 
-		 Point3 p = new Point3(); 
-		 System.out.println(p); // {0.0, 0.0, 0.0} 
-		 System.out.println(p.x + " " + p.y + " " + p.z); // 0.0 0.0 0.0 
-		 p.x = 20.0; 
-		 p.y = 3.0; 
-		 p.z = 12.0; 
-		 System.out.println(p.x + " " + p.y + " " + p.z); // 20.0 3.0 12.0 
-		 
-		 System.out.println("Point 3D 2");
-		 Point3 p2 = new Point3(10.0, 20.0, 30.0); 
-		 System.out.println(p2.x + " " + p2.y + " " + p2.z); // 10.0 20.0 30.0 
-		 		 
-		 int w = 1000, h = 600; 
-		 JFrame window = new JFrame("Java load opencv"); 
-		 window.setSize(w, h); 
-		 window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-		 
-		 window.setLocationRelativeTo(null); 
-		 window.setVisible(true); 
 	}
 }
